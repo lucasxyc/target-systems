ed41ca98d8bba6d5a32e91a0936621d02feeec4f
Lucas <163694732+lucasxyc@users.noreply.github.com>
2024-10-01 13:12:07 -0300

    Add files via upload

---
+++ src/SOMA.java
@@ -0,0 +1,19 @@
+public class SOMA {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int K = 1;
+
+        while (K < indice) {
+            K = K + 1; soma = soma + K;
+            System.out.println(soma);
+        }
+    }
+}
+
+/* 3) Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA
+
+ Valor: 78
+ */
